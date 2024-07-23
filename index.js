d71609e815ec26892d2ae9887aff3dcfb54a0b00
ZaeemRizwan2003 <210657@students.au.edu.pk>
2024-07-23 13:57:56 +0500

    line

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const mongoose = require('mongoose');
 
 const contactRoutes = require('./routes/contact.routes.js')
 const updateRoutes = require('./routes/updates.routes.js')
-const blogRoutes=require('./routes/blog.route.js')
+const blogRoutes = require('./routes/blog.route.js')
 const studentRoutes = require('./routes/student.routes.js');
 const startupRoutes = require('./routes/startup.routes.js');
-const ecommerceRoutes = require('./routes/ecommerce.routes.js'); 
-const digitalServiceRoutes = require('./routes/digitalservice.routes.js'); 
-const podcastRoutes = require('./routes/podcast.routes.js'); 
+const ecommerceRoutes = require('./routes/ecommerce.routes.js');
+const digitalServiceRoutes = require('./routes/digitalservice.routes.js');
+const podcastRoutes = require('./routes/podcast.routes.js');
 
-const learningpathRoutes = require ('./routes/learningpath.routes.js')
+const learningpathRoutes = require('./routes/learningpath.routes.js')
 
-const userRoutes=require('./routes/user.routes.js');
+const userRoutes = require('./routes/user.routes.js');
 
 var app = express();
 dotenv.config();
@@ -30,19 +30,20 @@ mongoose.connect(uri)
   .catch(err => console.error('Error occurred in connecting to MongoDB', err));
 
 app.use('/blog', blogRoutes);
-app.use('/ecommerce', ecommerceRoutes); 
+app.use('/ecommerce', ecommerceRoutes);
 app.use('/podcast', podcastRoutes);
-app.use('/digitalservice',digitalServiceRoutes);
-app.use('/users',userRoutes);
-app.use('/learningpath',learningpathRoutes);
-app.use('/startup',startupRoutes);
-app.use('/student',studentRoutes);
+app.use('/digitalservice', digitalServiceRoutes);
+app.use('/users', userRoutes);
+app.use('/learningpath', learningpathRoutes);
+app.use('/startup', startupRoutes);
+app.use('/student', studentRoutes);
 app.use('/contact', contactRoutes);
+app.use('/update', updateRoutes);
 app.use('/update', updateRoutes);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
+//adding comment 
 module.exports = app;
 
 
